b7b20dfdd526863d4d4807b36902d869ef033629
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2024-10-01 22:52:50 +0100

    COmeçar dropzone

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
+import Dropzone from "@/components/Dropzone";
+
 export default function Home() {
   return (
-    <div className="space-y-16 pb-8">
+    <div className="space-y-16 pb-8 px-12 xl:px-44 2xl:px-52">
       {/* Title + Desc */}
       <div className="space-y-6">
         <h1 className="text-3xl md:text-5xl font-medium text-center">
           Online File Converter
         </h1>
-        <p className="text-muted-foreground text-md md:text-lg text-center px-12 xl:px-44 2xl:px-52">
+        <p className="text-muted-foreground text-md md:text-lg text-center">
           Welcome to Convertio, your simple and efficient tool for converting
           files between different formats! We support a wide variety of file
           types, such as images, audio and video. With an intuitive interface,
@@ -17,6 +19,7 @@ export default function Home() {
       </div>
 
       {/* Upload Box */}
+      <Dropzone />
     </div>
   );
 }
---
+++ src/components/Dropzone.jsx
@@ -0,0 +1,149 @@
+"use client";
+
+import React, { useState } from "react";
+import ReactDropzone from "react-dropzone";
+import { FiUploadCloud } from "react-icons/fi";
+import { LuFileSymlink } from "react-icons/lu";
+import { useToast } from "@/hooks/use-toast"
+
+const extensions = {
+  image: [
+    "jpg",
+    "jpeg",
+    "png",
+    "gif",
+    "bmp",
+    "webp",
+    "ico",
+    "tif",
+    "tiff",
+    "svg",
+    "raw",
+    "tga",
+  ],
+  video: [
+    "mp4",
+    "m4v",
+    "mp4v",
+    "3gp",
+    "3g2",
+    "avi",
+    "mov",
+    "wmv",
+    "mkv",
+    "flv",
+    "ogv",
+    "webm",
+    "h264",
+    "264",
+    "hevc",
+    "265",
+  ],
+  audio: ["mp3", "wav", "ogg", "aac", "wma", "flac", "m4a"],
+};
+
+function Dropzone() {
+  const { toast } = useToast();
+  const [is_hover, setIsHover] = useState(false);
+
+  const accepted_files = {
+    "image/*": [
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".gif",
+      ".bmp",
+      ".webp",
+      ".ico",
+      ".tif",
+      ".tiff",
+      ".raw",
+      ".tga",
+    ],
+    "audio/*": [],
+    "video/*": [],
+  };
+
+  const handleUpload = (data) => {
+    handleExitHover();
+    setFiles(data);
+    const tmp = [];
+    data.forEach((file) => {
+      const formData = new FormData();
+      tmp.push({
+        file_name: file.name,
+        file_size: file.size,
+        from: file.name.slice(((file.name.lastIndexOf(".") - 1) >>> 0) + 2),
+        to: null,
+        file_type: file.type,
+        file,
+        is_converted: false,
+        is_converting: false,
+        is_error: false,
+      });
+    });
+    setActions(tmp);
+  };
+
+  const handleHover = () => setIsHover(true);
+  const handleExitHover = () => setIsHover(false);
+
+  return (
+    <ReactDropzone
+      onDrop={handleUpload}
+      onDragEnter={handleHover}
+      onDragLeave={handleExitHover}
+      accept={accepted_files}
+      onDropRejected={() => {
+        handleExitHover();
+        toast({
+          variant: "destructive",
+          title: "Error uploading your file(s)",
+          description: "Allowed Files: Audio, Video and Images.",
+          duration: 5000,
+        });
+      }}
+      onError={() => {
+        handleExitHover();
+        toast({
+          variant: "destructive",
+          title: "Error uploading your file(s)",
+          description: "Allowed Files: Audio, Video and Images.",
+          duration: 5000,
+        });
+      }}
+    >
+      {({ getRootProps, getInputProps }) => (
+        <div
+          {...getRootProps()}
+          className="bg-background h-72 lg:h-80 xl:h-96 rounded-3xl shadow-sm border-secondary border-2 border-dashed cursor-pointer flex items-center justify-center"
+        >
+          <input {...getInputProps()} />
+          <div className="space-y-4 text-foreground">
+            {is_hover ? (
+              <>
+                <div className="justify-center flex text-6xl">
+                  <LuFileSymlink />
+                </div>
+                <h3 className="text-center font-medium text-2xl">
+                  Yes, right there
+                </h3>
+              </>
+            ) : (
+              <>
+                <div className="justify-center flex text-6xl">
+                  <FiUploadCloud />
+                </div>
+                <h3 className="text-center font-medium text-2xl">
+                  Click, or drop your files here
+                </h3>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </ReactDropzone>
+  );
+}
+
+export default Dropzone;
