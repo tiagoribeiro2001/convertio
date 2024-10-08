8ed8da6184e2db2789cb4f933d3002c54524360b
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2024-10-08 21:04:07 +0100

    util functions

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata = {
   title: "Convertio",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Toaster />
         {children}
       </body>
     </html>
